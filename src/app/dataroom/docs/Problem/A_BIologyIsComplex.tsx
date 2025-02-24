29fb5b7168db902d62f2682ed21d4f6d542e7c5c
stenhold <doctorduality@gmail.com>
2025-02-24 13:33:27 -0800

    biology is complex finished mostly

--- src/app/dataroom/docs/Problem/A_BIologyIsComplex.tsx
+++ src/app/dataroom/docs/Problem/A_BIologyIsComplex.tsx
@@ -19,61 +19,41 @@ const A_BiologyIsComplex: React.FC = () => {
       </p>
       
 
-      <p className="roboto-slab-light text-base text-verydark leading-relaxed">
-
-      </p>
-      
-      <p className="mt-4 roboto-slab-light text-base text-verydark leading-relaxed">
-
-      </p>
-
-      
-      <p className="roboto-slab-light text-base text-verydark mb-4 mt-10 leading-relaxed">
-
-      </p>
-
-      <p className="roboto-slab-light text-base text-verydark mb-2 mt-10 leading-relaxed">
-
-      </p>
-
-      <div className="w-full mb-16">
-
-      </div>
-
-      <p className="roboto-slab-light text-base text-verydark mb-4 leading-relaxed">
-
-      </p>
-
-      <div className="w-full mb-16">
-
-      </div>
-
-      <p className="roboto-slab-light text-base text-verydark mb-4 leading-relaxed">
-
-      </p>
-
-      <div className="w-full mb-20">
-        <Image
-          src="/images/dataroom_images/zebrafish_radial.png"
-          alt="IO-healthy-disease"
-          width={500}
-          height={400}
-          className="object-contain"
-        />
+      <div className="w-full mb-12">
+        <figure className="text-left">
+          <Image
+            src="/images/dataroom_images/zebrafish_radial.png"
+            alt="IO-healthy-disease"
+            width={500}
+            height={400}
+            className="object-contain"
+          />
+          <figcaption className="roboto-slab-light text-xsm text-gray-dark mt-4">
+            An approximate representation of the major gene expression pathway cascades of zebrafish. 
+          </figcaption>
+        </figure>
       </div>
 
 
       <p className="roboto-slab-light text-base text-verydark mb-4 leading-relaxed">
-        
+        <strong> The history of bioengineering is checkered with cycles of discovery
+         and false confidence. </strong> 
+          A century of attempts at using classical engineering approaches toward
+          reliable health improvements has produced mixed results. 
+          For the number of research directions biologists have explored, few have 
+          resulted in reliable therapeutics. Vaccines, antibiotics, 
+          and small-molecules are the most successful examples, arguably the results 
+          of trial and error, directional luck, and low-hanging fruit more than 
+          bioengineering confidence and strategic intelligence.
       </p>
 
       <p className="roboto-slab-light text-base text-verydark mb-4 leading-relaxed">
-
-      </p>
-
-      <p className="roboto-slab-light text-base text-verydark mb-10 leading-relaxed">
-
-      </p>
+         <strong>We believe AI-based approaches represent 
+         a genuinely new paradigm of progress for bioengineers. </strong> 
+         No longer just a linear understanding of biological cause and effect; instead,
+         a fundamentally nonlinear set of approaches capable of capturing and modeling 
+         system-wide variables of connection.
+      </p> 
 
       <p className="roboto-slab-light text-base text-verydark mb-10 leading-relaxed">
 
