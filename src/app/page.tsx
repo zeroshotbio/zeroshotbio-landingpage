65390c8bb536cabb47f9f09529a966a5338841e2
Steven ten Holder <steve.tenholder@gmail.com>
2025-07-28 08:24:18 -0700

    updated landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ const DesktopContent = () => (
       </p>
       <p className="roboto-slab-regular text-xsm sm:text-base md:text-m text-gray-semidark leading-snug mb-4 sm:whitespace-normal">
         At <strong>zeroshot bio</strong>,
-        we create custom fine-tuning pipelines that translate zebrafish drug-exposure experiments
+        we create AI learning pipelines that translate zebrafish drug exposure data
         into human pre-clincal insights.
       </p>
       <p className="roboto-slab-regular text-xsm sm:text-base md:text-m text-gray-semidark leading-snug mb-2 sm:whitespace-normal">
@@ -115,7 +115,7 @@ const MobileContent = () => (
       </p>
       <p className="roboto-slab-regular text-xsm sm:text-base md:text-m text-gray-semidark leading-snug mb-4 sm:whitespace-normal">
         At <strong>zeroshot bio</strong>,
-        we create custom fine-tuning pipelines that translate zebrafish drug-exposure experiments
+        we create AI learning pipelines that translate zebrafish drug exposure data
         into human pre-clincal insights.
       </p>
       <p className="roboto-slab-regular text-xsm sm:text-base md:text-m text-gray-semidark leading-snug mb-2 sm:whitespace-normal">
@@ -136,18 +136,6 @@ const MobileContent = () => (
           <button className="group relative w-full px-6 py-3 border border-gray-400 text-gray-semidark roboto-slab-regular text-sm hover:border-gray-800 transition-all duration-300 ease-in-out">
             <span className="relative z-10 flex items-center justify-center">
               <span>Demo: Embedding Space Visualization</span>
-              <svg className="ml-2 w-4 h-4 opacity-0 transform translate-x-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all duration-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M14 5l7 7m0 0l-7 7m7-7H3" />
-              </svg>
-            </span>
-            <div className="absolute inset-0 bg-gray-50 transform origin-left scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></div>
-          </button>
-        </Link>
-
-        <Link href="/b_cross_species_viz" className="w-full">
-          <button className="group relative w-full px-6 py-3 border border-gray-400 text-gray-semidark roboto-slab-regular text-sm hover:border-gray-800 transition-all duration-300 ease-in-out">
-            <span className="relative z-10 flex items-center justify-center">
-              <span>Demo: Ortholog Network Visualization</span>
               <svg className="ml-2 w-4 h-4 opacity-0 transform translate-x-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all duration-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M14 5l7 7m0 0l-7 7m7-7H3" />
               </svg>
